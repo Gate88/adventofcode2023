5e4bd0443d34c89fd8b7b6b636247376c7cdf419
John Williams <zmaniac@gmail.com>
2024-05-05 01:17:51 -0400

    day01

---
+++ src/day01.rs
@@ -0,0 +1,62 @@
+use regex::Regex;
+
+pub const DAY01_INPUT: &'static str = include_str!(r"..\input\day01.txt");
+
+pub fn part1() -> i32 {
+    let input = DAY01_INPUT;
+    let re = Regex::new(r"^[^\d]*(\d).*(\d)[^\d]*$|^[^\d]*(\d)[^\d]*$").unwrap();
+    input.lines().map(|line| {
+        let captures = re.captures(line).unwrap();
+
+        let a = match captures.get(1) {
+            Some(m) => m,
+            None => captures.get(3).unwrap(),
+        }.as_str();
+
+        let b = match captures.get(2) {
+            Some(m) => m,
+            None => captures.get(3).unwrap(),
+        }.as_str();
+
+        format!("{a}{b}").parse::<i32>().unwrap()
+    }).sum()
+}
+
+pub fn part2() -> i32 {
+    let input = DAY01_INPUT;
+    let n_strings = r"one|two|three|four|five|six|seven|eight|nine";
+    let rev_n_strings = rev(n_strings);
+    let reg = Regex::new(&format!("\\d|{n_strings}")).unwrap();
+    let reg_rev = Regex::new(&format!("\\d|{rev_n_strings}")).unwrap();
+
+    input.lines().map(|line| {
+        let first = reg.find(line).unwrap().as_str();
+        let line_reverse = rev(line);
+        let last = reg_rev.find(&line_reverse).unwrap().as_str();
+
+        let first = match_to_digit(first);
+        let last_rev = rev(last);
+        let last = match_to_digit(&last_rev);
+
+        format!("{first}{last}").parse::<i32>().unwrap()
+    }).sum()
+}
+
+fn rev(s: &str) -> String {
+    return s.chars().rev().collect::<String>();
+}
+
+fn match_to_digit(s: &str) -> &str {
+    match s {
+        "one" => "1",
+        "two" => "2",
+        "three" => "3",
+        "four" => "4",
+        "five" => "5",
+        "six" => "6",
+        "seven" => "7",
+        "eight" => "8",
+        "nine" => "9",
+        s => s,
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod day01;
+
+fn main() {
+    main_d02();
+}
+
+#[allow(dead_code)]
+fn main_d01() {
+    let p1 = day01::part1();
+    println!("part1: {p1}");
+    let p2 = day01::part2();
+    print!("part2: {p2}");
+}
+
+fn main_d02() {
+
+}
