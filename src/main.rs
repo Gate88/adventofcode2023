b14122aee8807e5327f7f91ac8e8f76bb30f8b5a
John Williams <zmaniac@gmail.com>
2024-05-06 00:16:21 -0400

    day04 part1 & 2

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,24 @@ use std::env::args;
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 mod grouper;
 
 fn main() {
-    let s = args().nth(1).unwrap_or("3".to_owned());
+    let s = args().nth(1).unwrap_or("4".to_owned());
     println!("== Day{s} ==");
     match s.as_str() {
         "1" => main_d01(),
         "2" => main_d02(),
         "3" => main_d03(),
+        "4" => main_d04(),
         _ => panic!("not a valid day")
     };
+}
+
+fn main_d04() {
+    day04::part1();
+    day04::part2();
 }
 
 fn main_d03() {
